17926a7984873948d4d4a3857510ddf84caa48be
PavelBazilevich <102550003+PavelBazilevich@users.noreply.github.com>
2022-08-12 01:54:18 +0300

    ГОТОВО 1 ЗАВДАННЯ

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryEl = document.querySelector(".gallery")
+
+
+const resizeImage = event => {
+      if (event.target.nodeName !== "IMG") {
+    return;
+  }
+    event.preventDefault()
+  const modal = basicLightbox.create(
+    `<img width="1400" height="900" src="${event.target.dataset.source}"> `
+  ).show();
+    
+}
+
+const closeModal = event => {
+    if (event.key !== 'Escape' && basicLightbox.visible()) {
+    return;
+    }
+// ПЕРЕВІРКУ ЗРОБИВ ЗАЛИШИЛОСЬ ДОДАТИ ФУНКЦІОНАЛ ЗАКРИТТЯ ПО КНОПЦІ ЕСКЕЙП
+    
+     
+console.log(event)
+}
+
+
+galleryEl.addEventListener('click', resizeImage)
+galleryEl.addEventListener('keydown', closeModal)
+
+const itemGallaryMarkup = createItemGallaryMarkup(galleryItems)
+galleryEl.innerHTML = itemGallaryMarkup;
+
+function createItemGallaryMarkup(items) {
+    return items.map( ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`).join('');
+}
+
